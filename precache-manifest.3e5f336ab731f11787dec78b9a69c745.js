889bf7df6b41a78d27a4a0a62932ca65d612900a
Smihotur Roman <smikhotur@gmail.com>
2021-04-12 16:46:33 +0300

    Updates

--- precache-manifest.3e5f336ab731f11787dec78b9a69c745.js
+++ precache-manifest.3e5f336ab731f11787dec78b9a69c745.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3224c98cd22b3b68a4fd51d747676b1f",
+    "revision": "2ec12aa5f06b6119238f5f151b673637",
     "url": "./index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.f6a27176.chunk.css"
   },
   {
-    "revision": "a574d11c77e51285e7db",
-    "url": "./static/css/main.78e859e7.chunk.css"
+    "revision": "e614de799cfc3bbd5333",
+    "url": "./static/css/main.9a555762.chunk.css"
   },
   {
     "revision": "873ed4786507398d9eba",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.3eb1955b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a574d11c77e51285e7db",
-    "url": "./static/js/main.4cdee20d.chunk.js"
+    "revision": "e614de799cfc3bbd5333",
+    "url": "./static/js/main.cf77ae2b.chunk.js"
   },
   {
     "revision": "3cfb45b61c015a055433",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5851ed9c1c49c120ae643495e74419fa.js"
+  "./precache-manifest.3e5f336ab731f11787dec78b9a69c745.js"
 );
 
 self.addEventListener('message', (event) => {
